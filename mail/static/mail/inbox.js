7967701d8706fb5b4630b49308f4b5e31cafe353
Chris Cho <chriskccho@gmail.com>
2021-07-17 01:24:12 -0300

    added js for composing emails, sent, and inbox

--- mail/static/mail/inbox.js
+++ mail/static/mail/inbox.js
@@ -20,6 +20,34 @@ function compose_email() {
   document.querySelector('#compose-recipients').value = '';
   document.querySelector('#compose-subject').value = '';
   document.querySelector('#compose-body').value = '';
+
+  document.querySelector('#compose-form').onsubmit = function() {
+    const reci = document.querySelector('#compose-recipients').value
+    const subj = document.querySelector('#compose-subject').value
+    const body = document.querySelector('#compose-body').value
+    fetch('emails', {
+      method: 'POST',
+      body: JSON.stringify({
+        recipients: reci,
+        subject: subj,
+        body: body
+      }),
+    })
+    .then(response => response.json())
+    .then(result => {
+      if(result.hasOwnProperty('error')) {
+        alert(result.error);
+      }
+      else {
+        alert(result.message);
+      }
+
+    });
+    document.querySelector('#compose-subject').value = '';
+    document.querySelector('#compose-recipients').value = '';
+    document.querySelector('#compose-body').value = '';
+    return false; 
+  }
 }
 
 function load_mailbox(mailbox) {
@@ -30,4 +58,33 @@ function load_mailbox(mailbox) {
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
+
+  fetch(`/emails/${mailbox}`)
+  .then(response => response.json())
+  .then(emails => {
+    console.log(emails);
+    for (let i = 0; i < emails.length; i++) {
+      var sender = document.createElement('div');
+      sender.setAttribute('class','component sender');
+      sender.innerHTML = `${emails[i].sender}`;
+      var subject = document.createElement('div');
+      subject.setAttribute('class','component')
+      subject.innerHTML = `${emails[i].subject}`;
+      var timestamp = document.createElement('div');
+      timestamp.setAttribute('class','component time');
+      timestamp.innerHTML = `${emails[i].timestamp}`;
+      var eachEmail = document.createElement('div');
+      eachEmail.setAttribute('class','each-email');
+      eachEmail.addEventListener('click', function() {
+        console.log('this element has been clicked!');
+      });
+      eachEmail.append(sender);
+      eachEmail.append(subject);
+      eachEmail.append(timestamp);
+      console.log(eachEmail)
+      document.querySelector('#emails-view').append(eachEmail)
+    }
+    
+  });
+  
 }
